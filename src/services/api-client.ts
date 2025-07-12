3077bd7b8fa6c3a971614943b61c8845581ac621
Mohsen Samiee <Mo.Samiee@Outlook.com>
2025-07-12 12:11:09 -0400

    Refactor: Transfer rawg key to .env File

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "573f208686b14517a18a44283d381c80",
+    key: import.meta.env.VITE_RAWG_KEY,
   },
 });
