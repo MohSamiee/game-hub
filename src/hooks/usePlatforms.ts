ce07eb66f7f1e1ffaf34eaff5915e20ff18a3cdf
Mohsen Samiee <Mo.Samiee@Outlook.com>
2025-07-12 09:57:12 -0400

    Ship the platforms with app

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,16 @@
 import useData from "./useData";
 import { Platform } from "./useGames";
-
+import platforms from "../data/Platform";
 interface Platfor {
   id: number;
   name: string;
   slug: string;
 }
-const usetPlatforms = () => useData<Platform>("platforms/lists/parents");
+//const usetPlatforms = () => useData<Platform>("platforms/lists/parents");
+const usetPlatforms = () => ({
+  data: platforms,
+  isLoading: false,
+  error: null,
+});
+
 export default usetPlatforms;
