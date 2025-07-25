724f74bca8f5775ace2884cb1599f47dfea3d274
Mohsen Samiee <Mo.Samiee@Outlook.com>
2025-07-25 10:40:58 -0400

    Fetch the platforms with react-query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,16 +1,22 @@
-import useData from "./useData";
-import { Platform } from "./useGames";
+import { FetchResponse } from "./useData";
 import platforms from "../data/Platform";
-interface Platfor {
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+interface Platform {
   id: number;
   name: string;
   slug: string;
 }
-//const usetPlatforms = () => useData<Platform>("platforms/lists/parents");
-const usetPlatforms = () => ({
-  data: platforms,
-  isLoading: false,
-  error: null,
-});
+
+const usetPlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usetPlatforms;
