1b71a1e9ddd27a9d98aecfb976de4bf29ad12930
Mohsen Samiee <Mo.Samiee@Outlook.com>
2025-08-01 11:49:40 -0400

    Refactor- replace staleTime values with ms utility for consistency

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,8 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 import { GameQuery } from "../App";
-import APIClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
+import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
-
 export interface Game {
   id: number;
   name: string;
@@ -29,6 +28,6 @@ const useGames = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
-    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    staleTime: ms("24h"),
   });
 export default useGames;
